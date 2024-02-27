1b952df238ef02472fc03c5c5bb08aaa73685dc0
Dharshan <dharshan@selvisoftware.com>
2024-02-27 13:04:45 +0530

    github login and credential authentication added

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,37 @@
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        }),
+
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email:",
+                    type: "email",
+                    placeholder: "Enter email"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "Enter your password"
+                },
+            },
+            async authorize(credentials) {
+                const user = { id: 1, email: "john@gmail.com", password: "123456" };
+                if (credentials?.email === user.email && credentials?.password === user.password) {
+                    return user
+                }
+                else {
+                    return null
+                }
+            }
+        })
+    ]
+};
---
+++ middleware.ts
@@ -0,0 +1,4 @@
+export { default } from 'next-auth/middleware'
+
+// it runes
+export const config = { matcher: ['/dashboard'] }
